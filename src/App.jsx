2c0dff6e4b90dc6eea0bd0dc394857b6159514d3
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-05-24 17:21:15 +0200

    fix for GithubUsers

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AlertClock } from "./Alertclock";
 import { Clock } from "./Clock";
 import { Color } from "./Color";
@@ -15,67 +16,58 @@ import { Todolist } from "./Todolist";
 import { Todolist2 } from "./Todolist2";
 import { Welcome } from "./Welcome";
 import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
+import { GithubUser } from "./GithubUser"; // Ensure the path is correct
+import { GithubUsers } from "./GithubUsers"; // Ensure the path is correct
 
 export function App() {
-const [language, setLanguage] = useState('en')
+  const [language, setLanguage] = useState("en");
 
-function handleSetLanguage(event) {
-  setLanguage(event.target.value)
-}
+  function handleSetLanguage(event) {
+    setLanguage(event.target.value);
+  }
 
   return (
     <div>
       <select onChange={handleSetLanguage} value={language}>
-      <option value={'it'}>IT</option>
-      <option value={'en'}>EN</option>
-
+        <option value={"it"}>IT</option>
+        <option value={"en"}>EN</option>
       </select>
-      
- <Container title={<h1>My Page to try</h1>}>
-      <LanguageContext.Provider value={language}>
-      <Hello />
-      <Message />
-      <hr />
-      <Welcome name="Greg" age={30} />
 
-      <AlertClock />
-
-      <CounterDisplay />
-
-      <Clock />
-      <MouseClicker />
-
-
-      <InteractiveWelcome />
-
-      <ControlledForm2 />
-
-      <MyUncontrolledLogin />
-
-      <MyList items={[
-          { id: 1, name: "Jane", age: 20 },
-          { id: 2, name: "Luke", age: 30 },
-          { id: 3, name: "Saul", age: 40 },
-          { id: 4, name: "Vince", age: 45 },
-        ]} />
-
-<Colors colors = {[
-        {id: 1, name: 'red'},
-        {id: 2, name: 'blue'},
-        {id: 3, name: 'white'},
-        {id: 4, name: 'yellow'},
-      ]} />
-
-      <Todolist initialTodos = {['Cook', 'Sleep', 'Play Music', 'Sport']} />
-      
-
-<Todolist2 />
-      </LanguageContext.Provider>
-
-    </Container>
+      <Container title={<h1>My Page to try</h1>}>
+        <LanguageContext.Provider value={language}>
+          <Hello />
+          <Message />
+          <hr />
+          <Welcome name="Greg" age={30} />
+          <GithubUser username="spreadnroll" />
+          <GithubUsers />
+          <AlertClock />
+          <CounterDisplay />
+          <Clock />
+          <MouseClicker />
+          <InteractiveWelcome />
+          <ControlledForm2 />
+          <MyUncontrolledLogin />
+          <MyList
+            items={[
+              { id: 1, name: "Jane", age: 20 },
+              { id: 2, name: "Luke", age: 30 },
+              { id: 3, name: "Saul", age: 40 },
+              { id: 4, name: "Vince", age: 45 },
+            ]}
+          />
+          <Colors
+            colors={[
+              { id: 1, name: "red" },
+              { id: 2, name: "blue" },
+              { id: 3, name: "white" },
+              { id: 4, name: "yellow" },
+            ]}
+          />
+          <Todolist initialTodos={["Cook", "Sleep", "Play Music", "Sport"]} />
+          <Todolist2 />
+        </LanguageContext.Provider>
+      </Container>
     </div>
-   
-    
   );
 }
