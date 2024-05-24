d098b33cceb8dd9b7fbadfd6f4fe429f9e06c292
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-05-24 20:50:42 +0200

    Custom Hooks - 3

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -1,34 +1,8 @@
-import { useEffect, useState } from "react";
+import { useGithubUser } from "./useGithubUser";
+import React from "react";
 
 export function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      if (!response.ok) {
-        throw new Error("User not found!");
-      }
-      const json = await response.json();
-      console.log(json);
-
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
+  const { data, loading, error } = useGithubUser(username);
 
   return (
     <div>
